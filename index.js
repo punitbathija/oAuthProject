cf3b5d863a21cb5dee02b1a19a66155d2749c82f
Punit Bathija <punit644@gmail.com>
2023-02-18 16:50:50 +0530

    App completed with errors Error while authenticating with google

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const auth = require("./routes/auth");
+const passportConfig = require("./passport/passport");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
 const app = express();
-require("./passport/passport");
 
-mongoose.connect("mongodb://127.0.0.1:27017/passport", () => {
-  mongoose.set("strictQuery", false);
-  console.log("DB Connected");
-});
+//connect to the DB
+mongoose.connect("mongodb://127.0.0.1:27017/passport", () =>
+  console.log("DB CONNECTED")
+);
 
-app.listen(4000, console.log("App is listening at PORT 4000"));
+app.use(
+  cookieSession({
+    maxAge: 3 * 24 * 60 * 60 * 1000,
+    keys: ["thisislcotokenkey"], // dotenv
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const isLoggedIn = (req, res, next) => {
+  if (!req.user) {
+    res.redirect("/auth/login");
+  }
+  next();
+};
 
 app.set("view engine", "ejs");
 app.use("/auth", auth);
 
-app.get("/", (req, res) => {
+app.get("/", isLoggedIn, (req, res) => {
   res.render("home");
 });
+
+app.listen(4000, () => console.log(`Server is running at port 4000...`));
---
+++ passport/passport.js
@@ -0,0 +1,49 @@
+const passport = require("passport");
+const User = require("../models/user");
+
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID:
+        "902608766151-65r0ksltbc6cmgtia1mc0oiko5ru4nd1.apps.googleusercontent.com",
+      clientSecret: "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",
+      callbackURL: "http://localhost:4000/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, next) => {
+      console.log("MY PROFILE", profile._json.email);
+      User.findOne({ email: profile._json.email }).then((user) => {
+        if (user) {
+          console.log("User already exits in DB", user);
+          next(null, user);
+          // cookietoken()
+        } else {
+          User.create({
+            name: profile.displayName,
+            googleId: profile.id,
+            email: profile._json.email,
+          })
+            .then((user) => {
+              console.log("New User", user);
+              next(null, user);
+              // cookietoken()
+            })
+            .catch((err) => console.log(err));
+        }
+      });
+
+      //   next();
+    }
+  )
+);
