5bc2d7b09120a04999063f8ecfe5d7022e7a4598
Punit Bathija <punit644@gmail.com>
2023-02-18 16:33:07 +0530

    Added middlewares and important files to implement passport js google auth

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const auth = require("./routes/auth");
 const app = express();
+require("./passport/passport");
+
+mongoose.connect("mongodb://127.0.0.1:27017/passport", () => {
+  mongoose.set("strictQuery", false);
+  console.log("DB Connected");
+});
 
 app.listen(4000, console.log("App is listening at PORT 4000"));
 
 app.set("view engine", "ejs");
+app.use("/auth", auth);
 
 app.get("/", (req, res) => {
   res.render("home");
